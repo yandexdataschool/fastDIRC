39e82646dc75d1a381d14130940de64117cada90
Nikita Kazeev <kazeevn@yandex-team.ru>
2018-06-22 19:48:13 +0200

    more photons

--- multiple_tracks_generator/dircfit_multiple.cpp
+++ multiple_tracks_generator/dircfit_multiple.cpp
@@ -123,8 +123,8 @@ int main(int nargs, char* argv[]) {
 	const float s_func_t = 1.0;
 	const float sfunc_sig = 1;
 
-	int n_phi_phots = 300000;
-	int n_z_phots = 4;
+	int n_phi_phots = 500000;
+	int n_z_phots = 5;
 
 	bool use_quartz_for_liquid = false;
 	bool three_seg_mirror = true;
