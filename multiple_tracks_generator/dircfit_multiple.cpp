d381fee3fcfdfd9b74a195ff74c0ade2729c34cd
Nikita Kazeev <kazeevn@yandex-team.ru>
2018-06-22 02:04:27 +0200

    unc re

--- multiple_tracks_generator/dircfit_multiple.cpp
+++ multiple_tracks_generator/dircfit_multiple.cpp
@@ -119,7 +119,7 @@ int main(int nargs, char* argv[]) {
 	const float s_func_t = 1.0;
 	const float sfunc_sig = 1;
 
-	int n_phi_phots = 1000000;
+	int n_phi_phots = 150000;
 	int n_z_phots = 4;
 
 	bool use_quartz_for_liquid = false;
